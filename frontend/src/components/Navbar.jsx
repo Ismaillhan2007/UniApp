2bb799c687c06136b1801725082f1bb892e390cd
Ismaillhan2007 <159274473+Ismaillhan2007@users.noreply.github.com>
2025-12-06 05:06:59 +0500

    Deleted nav links,now we have one page without header

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,8 +4,6 @@ function Navbar() {
     return (
         <nav className="bg-blue-600 p-4">
             <div className="flex gap-6">
-                <Link to="/" className="text-white font-bold">Главная</Link>
-                <Link to="/universities" className="text-white font-bold">Университеты</Link>
             </div>
         </nav>
     );
